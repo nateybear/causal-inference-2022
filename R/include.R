6147184ea440239240df98f0b50581c64a7780a8
Hannah Gardner <hannahgardner@Hannahs-MBP-25.lan>
2022-01-12 16:37:32 -0600

    .RProfile and include.R setup

---
+++ R/include.R
@@ -0,0 +1,30 @@
+# installs the librarian package if you don't have it
+if (!("librarian" %in% rownames(utils::installed.packages()))) {
+  install.packages("librarian")
+}
+
+# put all of the packages that you import here
+librarian::shelf( 
+  cran_repo = "https://cran.microsoft.com/", # Dallas, TX
+  quiet = TRUE,
+  stats, # https://stackoverflow.com/questions/26935095/r-dplyr-filter-not-masking-base-filter#answer-26935536
+  here,
+  kableExtra,
+  rlang,
+  ggthemes,
+  tidyverse,
+  janitor,
+  magrittr,
+  glue,
+  lubridate,
+  haven,
+  snakecase,
+  sandwich,
+  lmtest,
+  gganimate,
+  gapminder,
+  stargazer
+)
+
+# tell here where we are so we can use it elsewhere
+here::i_am("R/include.R")
