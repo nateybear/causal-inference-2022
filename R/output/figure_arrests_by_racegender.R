d81f1ef1f47d37969dfcdf3f29da369b9805aebc
Nathan Hattersley <nate.hattersley@gmail.com>
2022-01-19 08:16:07 -0600

    minimal theme is better

--- R/output/figure_arrests_by_racegender.R
+++ R/output/figure_arrests_by_racegender.R
@@ -11,7 +11,7 @@ read_csv(here("data/NLSY97_clean.csv")) %>%
       y = "Mean Arrests", 
       fill = "Gender",
       title = "Mean Number of Arrests in 2002 by Race and Gender") +
-    theme_fivethirtyeight() +
+    theme_minimal() +
     scale_fill_economist()
 
 ggsave(here("figures/arrests_by_racegender.png"), width=8, height=4.5)
